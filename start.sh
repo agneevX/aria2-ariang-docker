62a00e36be7bdbaf53e9af6777e05e78281fcee9
Agneev Mukherjee <19761269+agneevX@users.noreply.github.com>
2022-01-25 21:38:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-conf_path=/aria2/conf
-conf_copy_path=/aria2/conf-copy
-data_path=/aria2/data
+config_path=/config
+docker_config_copy=/docker-aria2.conf
+downloads_path=/downloads
 
 if [ ! -f $conf_path/aria2.conf ]; then
-	cp $conf_copy_path/aria2.conf $conf_path/aria2.conf
+	cp $docker_config_copy $config_path/aria2.conf
     if [ -n "$RPC_SECRET" ]; then
         printf '\nrpc-secret=%s\n' ${RPC_SECRET} >> $conf_path/aria2.conf
     fi
 fi
 
-touch $conf_path/aria2.session
+touch $config_path/aria2.session
 
 if [[ -n "$ARIA2RPCPORT" ]]; then
     echo "Changing rpc request port to $ARIA2RPCPORT"
@@ -27,8 +27,8 @@ if [[ -n "$PUID" && -n  "$PGID" ]]; then
     groupid=$PGID
 fi
 
-chown -R $userid:$groupid $conf_path
-chown -R $userid:$groupid $data_path
+chown -R $userid:$groupid $config_path
+chown -R $userid:$groupid $downloads_path
 
 caddy -quiet -conf /usr/local/caddy/Caddyfile &
-su-exec $userid:$groupid aria2c --conf-path="$conf_path/aria2.conf"
+su-exec $userid:$groupid aria2c --conf-path="$config_path/aria2.conf"
